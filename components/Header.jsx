2d0434a4f04d2a68879d606b3b4a40d077c5bd06
Zechworld <ariel.herrera.s@gmail.com>
2022-03-07 10:06:39 -0300

    Calendar picker

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,37 @@
 // Libraries
 import { useState } from 'react'
 import Image from 'next/image'
+import { DateRangePicker } from 'react-date-range'
 import {
   GlobeAltIcon,
   MenuIcon,
   UserCircleIcon,
+  UsersIcon,
   SearchIcon,
 } from '@heroicons/react/solid'
 
-// CSS Files 
-import "react-date-range/dist/styles.css"
-import "react-date-range/dist/theme/default.css"
+// CSS Files
+import 'react-date-range/dist/styles.css'
+import 'react-date-range/dist/theme/default.css'
 
 export default function Header() {
+  // Estados
   const [searchInput, setSearchInput] = useState('')
+  const [numGuests, setNumGuests] = useState(1)
+  const [startDate, setStartDate] = useState(new Date())
+  const [endDate, setEndDate] = useState(new Date())
+
+  // Functions
+  const handleSelectDate = (ranges) => {
+    setStartDate(ranges.selection.startDate)
+    setEndDate(ranges.selection.endDate)
+  }
+
+  const selectionRange = {
+    startDate: startDate,
+    endDate: endDate,
+    key: 'selection',
+  }
 
   return (
     <header className="sticky top-0 z-50 grid grid-cols-3 bg-white p-3 shadow-md md:p-4">
@@ -50,7 +68,30 @@ export default function Header() {
         </div>
       </div>
 
-      {searchInput && <div></div>}
+      {/* Date Selector */}
+      {searchInput && (
+        <div className="col-span-3 mx-auto mt-5 flex flex-col">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={['#518769']}
+            onChange={handleSelectDate}
+          />
+          <div className="mb-4 flex items-center border-b pb-2">
+            <h2 className="flex-grow text-2xl font-semibold">
+              Number of Guest
+            </h2>
+
+            <UsersIcon className="h-5" />
+            <input
+              className="w-12 pl-2 text-lg text-green-400 outline-none"
+              type="number"
+              value="numGuests"
+              onChange={(e) => setNumGuests(e.target.value)}
+            />
+          </div>
+        </div>
+      )}
     </header>
   )
 }
